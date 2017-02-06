e04dd18edb9d507154ab06310ddec12c64d9d17e
verasveras <lisaveras@gmail.com>
2017-02-06 12:14:28 -0500

    Project skeleton

---
+++ js/react/index.jsx
@@ -0,0 +1,7 @@
+import Main from './main';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log(document.getElementById('app'))
+
+ReactDOM.render(<Main />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict';
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './js/react/index.jsx',
+  output: {
+    path: __dirname,
+    filename: './js/bundle.js'
+  },
+  watch: true,
+  module: {
+   loaders: [
+     {
+       test: /\.jsx/,
+       exclude: /node_modules/,
+       loader: 'babel-loader',
+       query: {
+         presets: ['react', 'es2015']
+       }
+     }
+   ]
+ },
+ resolve: {
+   extensions: ['', '.js', '.jsx', '.es6']
+ }
+}
